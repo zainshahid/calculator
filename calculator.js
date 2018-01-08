144050d00427aca800789be604a76534901e3779
zain shahid <zainulabideenshahid@gmail.com>
2018-01-08 07:49:03 -0500

    added print line to calculator

--- calculator.js
+++ calculator.js
@@ -14,3 +14,7 @@ function subtract(a,b) {
 function divide(a,b) { 
 	return (a / b)
 }
+
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
